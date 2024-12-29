6a9e3739337837e4f57f2c0a4d5a05ebc8edf51f
Panha32 <panharavuth2526@gmail.com>
2024-12-29 22:16:12 +0700

    add qrcode to contact

--- assets/js/qrCodeContact.js
+++ assets/js/qrCodeContact.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Email QR Code
     new QRCode(document.getElementById("email-qr"), {
-        text: "mailto:abc123@gmail.com",
+        text: "mailto:camezgo@info.com",
         width: 128,
         height: 128,
     });
 
     // Phone QR Code
     new QRCode(document.getElementById("phone-qr"), {
-        text: "tel:+1234567890",
+        text: "tel:+855 23 99 999",
         width: 128,
         height: 128,
     });
 
     // Location QR Code
     new QRCode(document.getElementById("location-qr"), {
-        text: "https://g.co/kgs/FxALYuE",
+        text: "https://maps.app.goo.gl/YDWQhLu1Nn1ejmdL7",
         width: 128,
         height: 128,
     });
